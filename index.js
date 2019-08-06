498ba0a97d24a1684c7d273b6c3293b88a5f704b
Sachin Singh <ssingh259@sapient.com>
2019-08-06 16:51:39 +0530

    Completed boilerplate first draft

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 #!/usr/bin/env node
 const fs = require('fs-extra');
-const root = process.cwd();
+const colors = require('colors');
 const inquirer = require('inquirer');
+const childProcess = require('child_process');
+const Spinner = require('cli-spinner').Spinner;
 const copySourceFiles = require('./utils/copySourceFiles');
+const root = process.cwd();
+
+const existingFiles = fs.readdirSync(root);
+if (existingFiles.length) {
+    console.log(colors.bold(colors.red('Workspace not empty!')));
+    return;
+}
 
 // Copy source files
+copySourceFiles(`${__dirname}/source/json`, 'json');
 copySourceFiles(`${__dirname}/source/src`, 'src');
+copySourceFiles(`${__dirname}/source/.eslintrc`, '.eslintrc');
+copySourceFiles(`${__dirname}/source/.gitignore`, '.gitignore');
+copySourceFiles(`${__dirname}/source/babel.config.js`, 'babel.config.js');
+copySourceFiles(`${__dirname}/source/plugin.test.js`, 'plugin.test.js');
+
 // Copy package.json file
 inquirer.prompt([
     {
@@ -25,7 +40,7 @@ inquirer.prompt([
     }
 ]).then(({ projectName, fileName, author }) => {
     // Read current content of package.json and rollup.config.js files
-    const packageJson = fs.readFileSync(`${__dirname}/source/package.json`);
+    const packageJson = fs.readFileSync(`${__dirname}/source/package.json`).toString();
     try {
         const packageJsonParsed = JSON.parse(packageJson);
         packageJsonParsed.name = projectName;
@@ -34,14 +49,34 @@ inquirer.prompt([
         // Write package.json file
         fs.writeFileSync(`${root}/package.json`, JSON.stringify(packageJsonParsed, null, 2));
     } catch (e) {
-        console.log('Package JSON file is not readable');
+        console.log(colors.bold(colors.red('Package JSON file is not readable')));
     }
     try {
         let rollupConfig = fs.readFileSync(`${__dirname}/source/rollup.config.js`).toString();
         rollupConfig = rollupConfig.replace(/\{fileName\}/g, fileName);
         fs.writeFileSync(`${root}/rollup.config.js`, rollupConfig);
     } catch (e) {
-        console.log('Rollup config file is not readable');
-        console.log(e);
+        console.log(colors.bold(colors.red('Rollup config file is not readable')));
     }
+    try {
+        let htmlFile = fs.readFileSync(`${__dirname}/source/dist/index.html`).toString();
+        htmlFile = htmlFile.replace(/\{fileName\}/g, fileName);
+        fs.mkdirSync(`${root}/dist`);
+        fs.writeFileSync(`${root}/dist/index.html`, htmlFile);
+    } catch (e) {
+        console.log(colors.bold(colors.red('HTML test file is not readable')));
+    }
+    // Run npm install
+    const spinner = new Spinner('Installing dependencies... (%s)');
+    spinner.setSpinnerString('|/-\\');
+    spinner.start();
+    const status = childProcess.spawn('npm', ['install']);
+
+    status.on('close', () => {
+        spinner.stop(true);
+        console.log(colors.bold(colors.green('Dependencies installed!\n\n')));
+        console.log(colors.bold(colors.green('======= Commands =======\n')));
+        console.log(colors.green(`${colors.black('"npm run start"'.bold)} to start the server.\n`));
+        console.log(colors.green(`${colors.black('"npm run build"'.bold)} to generate build files.\n`));
+    });
 });
--- utils/copySourceFiles.js
+++ utils/copySourceFiles.js
@@ -2,7 +2,6 @@ const fs = require('fs-extra');
 const root = process.cwd();
 
 // Copy source files
-module.exports = function copySourceFiles(sourcePath, distFolder) {
-    console.log(root);
+module.exports = function copySourceFiles(sourcePath, distFolder = '') {
     fs.copySync(sourcePath, `${root}/${distFolder}`);
 };
