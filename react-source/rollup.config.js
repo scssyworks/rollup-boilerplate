551332eabb98fa6ddc90d5736ad047ab082c2116
Sachin Singh <ssingh259@sapient.com>
2020-05-26 15:50:48 +0530

    Updated configuration to fix build issue

--- react-source/rollup.config.js
+++ react-source/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from '@rollup/plugin-replace';
-import babel from "rollup-plugin-babel";
+import babel from "@rollup/plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import { eslint } from 'rollup-plugin-eslint';
 import serve from 'rollup-plugin-serve';
