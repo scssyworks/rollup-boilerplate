3c948d15f36eb14543747398dc43c2f4cf14e18b
Sachin Singh <ssingh259@sapient.com>
2020-11-20 18:40:12 +0530

    Added ability to change versions of dependencies

--- utils/installDeps.js
+++ utils/installDeps.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const { projectTypes } = require('./constants');
 const { writeFileSync } = require('fs');
 const { root, isSubDirectory, argv } = require('./tArgs');
+const semver = require('semver');
 
 let npmProcess = `npm${/^win/.test(process.platform) ? '.cmd' : ''}`;
 
@@ -10,14 +11,43 @@ if (isSubDirectory) {
     npmProcess += ` --prefix ./${argv.name}`;
 }
 
-const commonDeps = [
+
+// Read versions file
+const versions = {};
+
+try {
+    Object.assign(versions, JSON.parse(
+        readFileSync(`${process.cwd()}/versions.json`, {
+            encoding: 'utf8'
+        }).toString()
+    ));
+} catch (e) {
+    console.log('[Version Check]: A valid "versions.json" file was NOT found. Default versions will be used.');
+    if (argv.logs) {
+        console.log(colors.blue('Create a "versions.json" file like the example shown below:'));
+        console.log(colors.blue(JSON.stringify({
+            '@babel/core': '^7.0.0',
+            '@babel/preset-env': '~7.0.0'
+        }, null, 2)));
+    }
+}
+
+function initializeVersions(deps) {
+    return deps.map(dep => {
+        const depName = dep.substring(0, dep.lastIndexOf('@'));
+        const depVersion = semver.valid(versions[depsName]) ? versions[depName] : dep.substring(dep.lastIndexOf('@') + 1);
+        return `${depName}@${depVersion}`;
+    });
+}
+
+const commonDeps = initializeVersions([
     '@babel/core@7',
     '@babel/preset-env@7',
     '@babel/plugin-proposal-class-properties@7',
     '@babel/plugin-proposal-private-methods@7',
     '@babel/plugin-transform-async-to-generator@7',
-    '@babel/plugin-transform-regenerator',
-    '@babel/plugin-transform-runtime',
+    '@babel/plugin-transform-regenerator@7',
+    '@babel/plugin-transform-runtime@7',
     '@rollup/plugin-babel@5',
     '@rollup/plugin-commonjs@15',
     '@rollup/plugin-node-resolve@9',
@@ -29,20 +59,20 @@ const commonDeps = [
     'rollup-plugin-livereload@2',
     'rollup-plugin-serve@1',
     'rollup-plugin-terser@7'
-];
+]);
 
-const reactDeps = [
+const reactDeps = initializeVersions([
     '@babel/preset-react@7',
     '@rollup/plugin-replace@2',
     'eslint-plugin-react@7'
-];
+]);
 
-const typescriptDeps = [
+const typescriptDeps = initializeVersions([
     '@babel/preset-typescript@7',
-    '@typescript-eslint/eslint-plugin@3',
-    '@typescript-eslint/parser@3',
-    'typescript@3'
-];
+    '@typescript-eslint/eslint-plugin@4',
+    '@typescript-eslint/parser@4',
+    'typescript@4'
+]);
 
 module.exports = function installDeps(projectType) {
     setImmediate(() => {
@@ -54,7 +84,7 @@ module.exports = function installDeps(projectType) {
         }
         let command = `${npmProcess} i -D ${deps.join(' ')}`;
         if (projectType === projectTypes.RJ) {
-            command += ` && ${npmProcess} i react react-dom`;
+            command += ` && ${npmProcess} i ${initializeVersions(['react@latest', 'react-dom@latest'].join(' '))}`;
         }
         childProcess.execSync(command, { stdio: [0, 1, 2] });
         if (projectType === projectTypes.RJ) {
